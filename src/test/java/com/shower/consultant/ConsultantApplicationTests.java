923b8e3802e4ce9a8b0ad75a0243bc69b42a7020
xxshowerxx <shower_pathos@outlook.com>
2025-09-14 15:59:00 +0800

    Initial commit

---
+++ src/test/java/com/shower/consultant/ConsultantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shower.consultant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
